d845de590f72a4e5e61a7edf776b8eecb2cb5001
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-05-16 02:17:00 +0600

    coffee making server is created

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Coffee Making Server Is Is Running")
+})
+
+app.listen(port,()=>{
+    console.log(`Coffee server is running on port: ${port}`);
+})
